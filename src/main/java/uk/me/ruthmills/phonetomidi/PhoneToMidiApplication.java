69cfa228256fd29a77a62805e5631daec4bbed7e
ruth <ruth@DESKTOP-HOVL7PT>
2025-06-02 19:36:04 +0100

    Initial commit of Spring Boot stuff.

---
+++ src/main/java/uk/me/ruthmills/phonetomidi/PhoneToMidiApplication.java
@@ -0,0 +1,13 @@
+package uk.me.ruthmills.phonetomidi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneToMidiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneToMidiApplication.class, args);
+	}
+
+}
